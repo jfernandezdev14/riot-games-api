0865e81d168fd60138b68d79b9d9fc13e54cf78a
Juan Sebastian Fernandez <juan.fernandez@trafilea.com>
2023-11-25 22:37:21 -0500

    Adding initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
